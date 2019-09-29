604557fadb2b81659364e43abea9030e27f06bce
DmitryKress <dmitry_job@bk.ru>
2019-09-29 20:53:28 +0300

    1

---
+++ script.js
@@ -0,0 +1,28 @@
+var product = prompt("Пожалуйста, введите день недели");
+
+switch (product) {
+  case "Monday":
+    alert("Working Day");
+    break;
+  case "Tuesday":
+    alert("Working Day");
+    break;
+  case "Wednesday":
+    alert("Working Day");
+    break;
+  case "Thursday":
+    alert("Working Day");
+    break;
+  case "Friday":
+    alert("Working Day");
+    break;
+  case "Saturday":
+    alert("Weekend");
+    break;
+  case "Sunday":
+    alert("Weekend");
+    break;
+  default:
+    alert("Can not define, please try another value");
+    break;
+}
